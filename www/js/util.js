7d7785e46c847c31bcd4b112a458bffa475866b3
Patrick Yurkiw <patrick.yurkiw@wattswater.com>
2022-09-08 23:53:50 -0700

    Refactor iFrames to instead use Javascript
    Move copy/pasted HTML into new addFleet, addBoat, addTimer modules.

---
+++ www/js/util.js
@@ -0,0 +1,112 @@
+/*************************************************************************************************
+ * File: util.js
+------------------------------------------------------------------------------------------------*/
+/*************************************************************************************************
+ * Add Boat.
+------------------------------------------------------------------------------------------------*/
+jQuery.fn.addBoat = function(name, id, timer) {
+    $(this).append(`
+        <tr>
+            <td class="tdundo">
+                <button class="button_undo" disabled id="${id}_undo" />
+                <img id="${id}_undo_img" src="icons/disabled_undo.png" />
+            </td>
+            <td class="AFleettd">
+                <span id='${id}_boat_name'>${name}</span>
+            </td>
+            <td>
+                <button class=button id='${id}_finish';>
+                    <img id="${id}_finish_img" src="icons/racing.png" />
+                </button>
+            </td>
+            <td>
+                <button class=button id="${id}_dnf" onclick="DNFBoat('${id}')"><img id="${id}_dnf_img" src="icons/dnf1.png" />
+            </td>
+            <td>
+                <span id="${id}_time_display">00:00:00:00</span><span hidden id="${id}_time_finish"></span>
+            </td>
+        </tr>
+    `);
+
+    // Undo Button
+    $(`#${id}_undo`).on('click', () => {
+        undo_result(id, timer);
+    });
+
+    // Finish Button
+    $(`#${id}_finish`).on('click', () => {
+        FinishBoat(id, timer);
+    });
+}
+
+/*************************************************************************************************
+ * Add Fleet.
+------------------------------------------------------------------------------------------------*/
+jQuery.fn.addFleet = function(name, id, boats) {
+    // Create HTML
+    $(this).append(`
+        <div class="stopwatch">
+            <h1>${name}</h1>
+
+            <div class="circle">
+                <span class="atime" id="${id}_display">00:00:00:00</span>
+            </div>
+            <div class="controls">
+                <span id='${id}_timer' class="buttonPlay"></span>
+                <button class="reload">
+                    <img id="${id}_reload" src="icons/Restart.png" />
+                </button>
+                <button class="buttonWrite">
+                    <img id="${id}_download" src="icons/write.png" />
+                </button>
+                <select name="course" id="course" class="box">
+                    <option value="Course 1">Course 1</option>
+                    <option value="Course 2">Course 2</option>
+                    <option value="Course 3">Course 3</option>
+                    <option value="Course 4">Course 4</option>
+                    <option value="Course 5">Course 5</option>
+                    <option value="Course 6">Course 6</option>
+                    <option value="Course 7">Course 7</option>
+                    <option value="Course 8">Course 8</option>
+                    <option value="Course 9">Course 9</option>
+                    <option value="Course 10">Course 10</option>
+                    <option value="Course 11">Course 11</option>
+                    <option value="Course 12">Course 12</option>
+                    <option value="Course 13">Course 13</option>
+                    <option value="Course 14">Course 14</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="results">
+            <table id='${id}'></table>
+        </div>
+    `);
+
+    // Create Timer
+    var timer = createTimer(`#${id}_timer`, id);
+
+    // Add Boats
+    for(var index = 0; index < boats.length; index++) {
+        $('#' + id).addBoat(boats[index], `${id}_${index}`, timer);
+    }
+
+    // On Reset
+    $(`#${id}_reload`).on('click', () => {
+        // Confirmation
+        if(reset(timer, 'Start New Race?')) {
+            // Clear Boats
+            $(`#${id}`).html('');
+
+            // Reload Boats
+            for(var index = 0; index < boats.length; index++) {
+                $(`#${id}`).addBoat(boats[index], `${id}_${index}`, timer);
+            }
+        }
+    });
+
+    // Download
+    $(`#${id}_download`).on('click', () => {
+        download(`${name[0]}_FLEET`, CSVResultsAsString(id, boats.length, name[0], timer.raceStart));
+    });
+}
--- www/script.js
+++ www/script.js
@@ -1,160 +1,175 @@
 // Convert time to a format of hours, minutes, seconds, and milliseconds
 
 function timeToString(time) {
-  let diffInHrs = time / 3600000;
-  let hh = Math.floor(diffInHrs);
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
 
-  let diffInMin = (diffInHrs - hh) * 60;
-  let mm = Math.floor(diffInMin);
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
 
-  let diffInSec = (diffInMin - mm) * 60;
-  let ss = Math.floor(diffInSec);
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
 
-  let diffInMs = (diffInSec - ss) * 100;
-  let ms = Math.floor(diffInMs);
+    let diffInMs = (diffInSec - ss) * 100;
+    let ms = Math.floor(diffInMs);
 
-  let formattedHH = hh.toString().padStart(2, "0");	
-  let formattedMM = mm.toString().padStart(2, "0");
-  let formattedSS = ss.toString().padStart(2, "0");
-  let formattedMS = ms.toString().padStart(2, "0");
+    let formattedHH = hh.toString().padStart(2, "0");	
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+    let formattedMS = ms.toString().padStart(2, "0");
 
-  return `${formattedHH}:${formattedMM}:${formattedSS}:${formattedMS}`;
+    return `${formattedHH}:${formattedMM}:${formattedSS}:${formattedMS}`;
 }
 
-// Declare variables to use in our functions below
+/*************************************************************************************************
+ * Start Timer.
+------------------------------------------------------------------------------------------------*/
+function start(timer) {
+    // changed this to time only for .csv file output	
+    timer.raceStart = getCurrentTime();
+    timer.startTime = Date.now() - timer.elapsedTime;
+    timer.timerInterval = setInterval(function printTime() {
+        timer.elapsedTime = Date.now() - timer.startTime;
+        timer.display.html(timeToString(timer.elapsedTime));
+    }, 10);
 
-let startTime;
-let elapsedTime = 0;
-let timerInterval;
-let raceStart = "No Race Started";
-
-// Create function to modify innerHTML
-
-function print(txt) {
-  document.getElementById("display").innerHTML = txt;
+    // Pause Button
+    $(timer.playButton ).hide();
+    $(timer.pauseButton).show();
 }
 
-// Create "start", "pause" and "reset" functions
+/*************************************************************************************************
+ * Pause Timer.
+------------------------------------------------------------------------------------------------*/
+function pause(timer) {
+    clearInterval(timer.timerInterval);
 
-function start() {
-  //raceStart = new Date();
-  // changed this to time only for .csv file output	
-  raceStart = getCurrentTime();
-  startTime = Date.now() - elapsedTime;
-  timerInterval = setInterval(function printTime() {
-    elapsedTime = Date.now() - startTime;
-    print(timeToString(elapsedTime));
-  }, 10);
-  showButton("PAUSE");
+    // Play Button
+    $(timer.playButton ).show();
+    $(timer.pauseButton).hide();
 }
 
-function pause() {
-  clearInterval(timerInterval);
-  showButton("PLAY");
+/*************************************************************************************************
+ * Reset Timer.
+------------------------------------------------------------------------------------------------*/
+function reset(timer, text) {
+    if(confirm(text)) {
+        clearInterval(timer.timerInterval);
+        timer.display.html("00:00:00:00");
+
+        // Play Button
+        $(timer.playButton ).show();
+        $(timer.pauseButton).hide();
+
+        return true;
+	  } else {
+        return false;
+    }
 }
 
-function reset() {
-if (confirm('Stop and clear timer?'))
-	{
-         clearInterval(timerInterval);
-         print("00:00:00:00");
-         elapsedTime = 0;
-         showButton("PLAY");
-	}
+/*************************************************************************************************
+ * Add Timer Object.
+------------------------------------------------------------------------------------------------*/
+function createTimer(id, prefix) {
+    // Add Buttons
+    $(id).append(`
+        <img id='${prefix}_playButton'  src="icons/play-button.png"  />
+        <img id='${prefix}_pauseButton' src="icons/pause-button.png" />
+        <img id='${prefix}_resetButton' src="icons/reset-button.png" />
+    `);
+
+    // Timer Object
+    var timer = {
+        startTime:     null,
+        elapsedTime:   0,
+        timerInterval: null,
+        raceStart:     'No Race Started',
+        playButton:    $(`#${prefix}_playButton`),
+        pauseButton:   $(`#${prefix}_pauseButton`),
+        resetButton:   $(`#${prefix}_resetButton`),
+        display:       $(`#${prefix}_display`)
+    };
+    
+    // Add Callbacks
+    timer.playButton .on('click', () => start(timer));
+    timer.pauseButton.on('click', () => pause(timer));
+    timer.resetButton.on('click', () => reset(timer, 'Stop and clear timer?'));
+
+    // Play Button
+    $(timer.playButton ).show();
+    $(timer.pauseButton).hide();
+
+    // Attach Object
+    $(id).data('timer', timer);
+
+    // Return Object
+    return timer;
 }
-
-// Create function to display buttons
-
-function showButton(buttonKey) {
-  const buttonToShow = buttonKey === "PLAY" ? playButton : pauseButton;
-  const buttonToHide = buttonKey === "PLAY" ? pauseButton : playButton;
-  buttonToShow.style.display = "block";
-  buttonToHide.style.display = "none";
-}
-// Create event listeners
-
-let playButton = document.getElementById("playButton");
-let pauseButton = document.getElementById("pauseButton");
-let resetButton = document.getElementById("resetButton");
-
-playButton.addEventListener("click", start);
-pauseButton.addEventListener("click", pause);
-resetButton.addEventListener("click", reset);
-
 
 // race scoring functions
+function FinishBoat(prefix, timer) {
+    let undo         = prefix + "_undo";
+    let undo_img     = prefix +	"_undo_img";
+    let finish       = prefix + "_finish";
+    let finish_img   = prefix + "_finish_img"; 
+    let dnf          = prefix + "_dnf";
+    let dnf_img      = prefix + "_dnf_img";
+    let time_display = prefix + "_time_display";
+    let time_finish  = prefix + "_time_finish";
 
-function FinishBoat(prefix) {
- let undo = prefix + "_undo";
- let undo_img = prefix +	"_undo_img";
- let finish = prefix + "_finish";
- let finish_img =  prefix + "_finish_img"; 
- let dnf =  prefix + "_dnf";
- let dnf_img =  prefix + "_dnf_img";
- let time_display =  prefix + "_time_display";
- let time_finish = prefix + "_time_finish"; 
- //alert("Finishing boat! id is: " + racing_button_id);
- document.getElementById(finish).disabled=true;
- document.getElementById(dnf).disabled=true;
- document.getElementById(finish_img).src="icons/finished.png";
- document.getElementById(dnf_img).src="icons/dnf_disabled.png";
- document.getElementById(time_display).textContent=timeToString(elapsedTime);
- document.getElementById(time_finish).textContent=getCurrentTime();
+    document.getElementById(finish).disabled=true;
+    document.getElementById(dnf).disabled=true;
+    document.getElementById(finish_img).src="icons/finished.png";
+    document.getElementById(dnf_img).src="icons/dnf_disabled.png";
+    document.getElementById(time_display).textContent=timeToString(timer.elapsedTime);
+    document.getElementById(time_finish).textContent=getCurrentTime();
 
-// enable undo button
-// disable undo button
- document.getElementById(undo).disabled=false;
- document.getElementById(undo_img).src="icons/undo.png";	
-
+    // enable undo button
+    // disable undo button
+    document.getElementById(undo).disabled=false;
+    document.getElementById(undo_img).src="icons/undo.png";	
 }
-
-
-
 
 function DNFBoat(prefix) {
- //alert("DNF boat! id is: " + dnf_button_id);
- let undo = prefix + "_undo";
- let undo_img = prefix +	"_undo_img";
- let finish = prefix + "_finish";
- let finish_img =  prefix + "_finish_img"; 
- let dnf =  prefix + "_dnf";
- let dnf_img =  prefix + "_dnf_img";
- let time_display =  prefix + "_time_display";
- document.getElementById(dnf).disabled=false;
- document.getElementById(finish).disabled=true;
- document.getElementById(dnf_img).src="icons/dnf.png";
- document.getElementById(finish_img).src="icons/dnf_racing.png";
- document.getElementById(time_display).textContent = " -- DNF --";
-// enable undo button
- document.getElementById(undo).disabled=false;
- document.getElementById(undo_img).src="icons/undo.png";
+    let undo         = prefix + "_undo";
+    let undo_img     = prefix +	"_undo_img";
+    let finish       = prefix + "_finish";
+    let finish_img   = prefix + "_finish_img"; 
+    let dnf          = prefix + "_dnf";
+    let dnf_img      = prefix + "_dnf_img";
+    let time_display = prefix + "_time_display";
+
+    document.getElementById(dnf).disabled=false;
+    document.getElementById(finish).disabled=true;
+    document.getElementById(dnf_img).src="icons/dnf.png";
+    document.getElementById(finish_img).src="icons/dnf_racing.png";
+    document.getElementById(time_display).textContent = " -- DNF --";
+    // enable undo button
+    document.getElementById(undo).disabled=false;
+    document.getElementById(undo_img).src="icons/undo.png";
 }
 
+function undo_result(prefix, timer) {
+    let undo         = prefix + "_undo";
+    let undo_img     = prefix +	"_undo_img";
+    let finish       = prefix + "_finish";
+    let finish_img   = prefix + "_finish_img"; 
+    let dnf          = prefix + "_dnf";
+    let dnf_img      = prefix + "_dnf_img";
+    let time_display = prefix + "_time_display";
+
+    // enable finish and DNF buttons and reset time
+    document.getElementById(finish).disabled=false;
+    document.getElementById(dnf).disabled=false;
+    document.getElementById(finish_img).src="icons/racing.png";
+    document.getElementById(dnf_img).src="icons/dnf1.png";
+    document.getElementById(time_display).textContent="00:00:00:00";
 
 
-function undo_result(prefix) {
-let undo = prefix + "_undo";
-let undo_img = prefix +	"_undo_img";
-let finish = prefix + "_finish";
-let finish_img =  prefix + "_finish_img"; 
-let dnf =  prefix + "_dnf";
-let dnf_img =  prefix + "_dnf_img";
-let time_display =  prefix + "_time_display";
-
-// enable finish and DNF buttons and reset time
-document.getElementById(finish).disabled=false;
-document.getElementById(dnf).disabled=false;
-document.getElementById(finish_img).src="icons/racing.png";
-document.getElementById(dnf_img).src="icons/dnf1.png";
-document.getElementById(time_display).textContent="00:00:00";
-
-
-// disable undo button
-document.getElementById(undo).disabled=true;
-document.getElementById(undo_img).src="icons/disabled_undo.png";
-
+    // disable undo button
+    document.getElementById(undo).disabled=true;
+    document.getElementById(undo_img).src="icons/disabled_undo.png";
 }	
-
 
 // file handling
 function download(filename, text) {
@@ -173,75 +188,77 @@ function download(filename, text) {
         var event = document.createEvent('MouseEvents');
         event.initEvent('click', true, true);
         pom.dispatchEvent(event);
-    }
-    else {
+    } else {
         pom.click();
     }
 }
 
 function getResultsAsString(start_boat, end_boat, fleet, startTime) {
-	let select = document.getElementById('course');
-	let course = select.options[select.selectedIndex].text;
-	let results_as_text = "";
-	let line = "\n--------------------------------------------------------------------------------\n";
-	results_as_text += new Date() + line;
-	results_as_text += "FLEET: "  + fleet + "\n";
-	results_as_text += "COURSE: "  + course + "\n";
-	results_as_text += "START: "  + startTime + line;
-	for (let i = start_boat; i <= end_boat; i++) {
-          results_as_text += document.getElementById("b" + i + "_boat_name").textContent.padEnd(30);
-	  results_as_text += "\t";
-	  results_as_text += document.getElementById("b" + i + "_time_display").textContent;
-	  results_as_text += "\n";
-	}
+    let select = document.getElementById('course');
+    let course = select.options[select.selectedIndex].text;
+    let results_as_text = "";
+    let line = "\n--------------------------------------------------------------------------------\n";
+    results_as_text += new Date() + line;
+    results_as_text += "FLEET: "   + fleet + "\n";
+    results_as_text += "COURSE: "  + course + "\n";
+    results_as_text += "START: "   + startTime + line;
 
-        return results_as_text;
+    for (let i = start_boat; i <= end_boat; i++) {
+        results_as_text += document.getElementById("b" + i + "_boat_name").textContent.padEnd(30);
+        results_as_text += "\t";
+        results_as_text += document.getElementById("b" + i + "_time_display").textContent;
+        results_as_text += "\n";
+    }
+
+    return results_as_text;
 }
 
 
-function CSVResultsAsString(start_boat, end_boat, fleet, startTime) {
-	let select = document.getElementById('course');
-	let course = select.options[select.selectedIndex].text;
-	let new_race = "+";
-	let results_as_text = "";
-	let header = "RaceNo,Fleet,Boat,Start,Finish";
-	let line = "\n--------------------------------------------------------------------------------\n";
-	results_as_text += header + "\n";
-	for (let i = start_boat; i <= end_boat; i++) {
-		// remove the + sign at the start of the line
-		// for all but the first line 
-		if (i > start_boat) {
-			new_race = "";
-		}
-          //RaceNo
-	  results_as_text += new_race;
-	  results_as_text += ",";
-          //Fleet
-	  results_as_text += fleet;
-	  results_as_text += ",";
-          //Boat
-	  results_as_text += document.getElementById("b" + i + "_boat_name").textContent;
-	  results_as_text += ",";
-          //Start
-	  results_as_text += startTime;
-	  results_as_text += ",";
-          //Finish
-	  results_as_text += document.getElementById("b" + i + "_time_finish").textContent;;
-	  results_as_text += "\n";
-	}
+function CSVResultsAsString(prefix, boats, fleet, startTime) {
+    let select = document.getElementById('course');
+    let course = select.options[select.selectedIndex].text;
+    let new_race = "+";
+    let results_as_text = "";
+    let header = "RaceNo,Fleet,Boat,Start,Finish";
+    let line = "\n--------------------------------------------------------------------------------\n";
+    results_as_text += header + "\n";
 
-        return results_as_text;
+	  for (var i = 0; i < boats; i++) {
+        // remove the + sign at the start of the line
+        // for all but the first line 
+        if (i > 0) {
+            new_race = "";
+        }
+
+        // RaceNo
+        results_as_text += new_race;
+        results_as_text += ",";
+
+        // Fleet
+        results_as_text += fleet;
+        results_as_text += ",";
+
+        // Boat
+        results_as_text += $(`#${prefix}_${i}_boat_name`).text();
+        results_as_text += ",";
+
+        // Start
+        results_as_text += startTime;
+        results_as_text += ",";
+
+        // Finish
+        results_as_text += $(`#${prefix}_${i}_time_finish`).text();
+        results_as_text += "\n";
+	  }
+
+    return results_as_text;
 }
-
 
 function getCurrentTime() {
+	  var date    = new Date;
+    var seconds = String(date.getSeconds()).padStart(2, '0');
+    var minutes = String(date.getMinutes()).padStart(2, '0');
+    var hour    = String(date.getHours()).padStart(2, '0');
 
-	var date = new Date;
-        //date.setTime(result_from_Date_getTime);
-        var seconds = String(date.getSeconds()).padStart(2, '0');
-        var minutes = String(date.getMinutes()).padStart(2, '0');
-        var hour = String(date.getHours()).padStart(2, '0');
-
-        return hour + ":" + minutes + ":" + seconds;
-
+    return hour + ":" + minutes + ":" + seconds;
 }
